fde9d99c9188680edb93522ff7b28ea8ebf3c140
Zhao Zhigen <zhaozhigen@STAT-ZHIGEN-M1PRO.local>
2024-03-21 11:04:02 -0400

    update

--- vimp.py
+++ vimp.py
@@ -11,25 +11,25 @@ class vimp:
     
   def get2(self):
     poly = PolynomialFeatures(degree=2)
-    X_poly = poly.fit_transform(X)
+    X_poly = poly.fit_transform(self.x)
   
   
     #Choosing technique to estimate conditional mean
     #Regress Y on X using same technique
     model = LinearRegression()
-    model.fit(X_poly, y)
+    model.fit(X_poly, self.y)
     u = model.predict(X_poly)
     
     
     #Regress u on X_-s using same technique
-    coefficients = np.polyfit(X[:,1], u, 2)
-    u1 = np.polyval(coefficients, X[:,1])
+    coefficients = np.polyfit(self.x[:,1], u, 2)
+    u1 = np.polyval(coefficients, self.x[:,1])
     
-    coefficients = np.polyfit(X[:,0], u, 2)
-    u2 = np.polyval(coefficients, X[:,0])
+    coefficients = np.polyfit(self.x[:,0], u, 2)
+    u2 = np.polyval(coefficients, self.x[:,0])
     
-    psi1 = 1 - (np.mean((y-u)**2)/np.var(y)) - (1 - np.mean((y - u1)**2)/np.var(y))
-    psi2 = 1 - (np.mean((y-u)**2)/np.var(y)) - (1 - np.mean((y - u2)**2)/np.var(y))
+    psi1 = 1 - (np.mean((self.y-u)**2)/np.var(self.y)) - (1 - np.mean((self.y - u1)**2)/np.var(self.y))
+    psi2 = 1 - (np.mean((self.y-u)**2)/np.var(self.y)) - (1 - np.mean((self.y - u2)**2)/np.var(self.y))
     
     return f"psi1 =",psi1,"and psi2 =",psi2
   
