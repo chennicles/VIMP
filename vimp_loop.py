9b726f1b505c21abb5c416645ea0c5b1cdc56829
Kyle Chen <164234327+kchen1031@users.noreply.github.com>
2024-04-21 15:14:54 -0400

    Update vimp_loop.py
    Added data splitting that gives VIMP cutoff for FDR level q

--- vimp_loop.py
+++ vimp_loop.py
@@ -4,12 +4,10 @@ from sklearn.linear_model import LinearRegression
 
 
 class vimp:
-    def __init__(self, y, x, d):
+    def get_vimp(self, y, x, d):
         self.y = y
         self.x = x
         self.d = d
-
-    def get(self):
         poly = PolynomialFeatures(degree=self.d)
         X_poly = poly.fit_transform(self.x)
 
@@ -42,3 +40,42 @@ class vimp:
             psi[i] = 1 - (residual_var / np.var(self.y)) - (1 - (mu_var / np.var(self.y)))
 
         return psi
+
+    def datasplit(self, q):
+        self.q = q
+        # Splitting data
+        y1 = self.y[:int(len(self.x) / 2)]
+        y2 = self.y[int(len(self.x) / 2):]
+
+        X1 = self.x[:int(len(self.x) / 2)]
+        X2 = self.x[int(len(self.x) / 2):]
+
+        # Estimating psi1 and psi2
+        v1 = self.get_vimp(y1, X1, self.d)
+        v2 = self.get_vimp(y2, X2, self.d)
+
+        psi1 = np.array(v1)
+        psi2 = np.array(v2)
+
+        # Calculate mirror stat
+        m = abs(psi1 + psi2) - abs(psi1 - psi2)
+
+        # Designate FDR level
+        # Assuming M contains p-values from multiple hypothesis tests
+        q = self.q  # Your designated FDR level
+
+        # Sort the mirror statistics in ascending order
+        sorted_m = np.sort(m)
+        n = len(sorted_m)
+
+        # Calculate the cutoff tau_q using BH procedure
+        tau_q = 0
+        for i, t in enumerate(sorted_m):
+            # Calculate the critical value for this p-value
+            critical_value = (i + 1) / n * q
+
+            # Find the largest p-value where the p-value is less than the critical value
+            if t <= critical_value:
+                tau_q = t
+
+        print("tau_q:", tau_q)
