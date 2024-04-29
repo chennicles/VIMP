1cbee6db52124c5fd1ca2aa92c4a368e2779c24c
Kyle Chen <164234327+kchen1031@users.noreply.github.com>
2024-04-29 18:04:31 -0400

    Update vimp_loop.py
    Changed regression method to cv elastic net

--- vimp_loop.py
+++ vimp_loop.py
@@ -1,45 +1,33 @@
 import numpy as np
-from sklearn.preprocessing import PolynomialFeatures
-from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import ElasticNetCV
 
 
 class vimp:
-    def __init__(self, y, x):
-        self.y = y
+    def get(self, y, x):
         self.x = x
+        self.y = y
+        # Transform the entire predictor matrix
 
-    def get(self, d):
-        self.d = d
-        poly = PolynomialFeatures(degree=self.d)
-        X_poly = poly.fit_transform(self.x)
+        # Fit the full model using ElasticNetCV
+        full_model = ElasticNetCV(cv=5, l1_ratio=[.1, .5, .7, .9, .95, .99, 1])
+        full_model.fit(self.x, self.y)
+        u = full_model.predict(self.x)
 
-        model = LinearRegression()
-        model.fit(X_poly, self.y)
-        u = model.predict(X_poly)
+        psi = []
 
-        nvar = self.x.shape[1]
-        psi = [0] * nvar
-        mu = [0] * nvar
+        # Loop over each predictor
+        for i in range(self.x.shape[1]):
+            X_s = np.delete(self.x, i, axis=1)
 
-        for i in range(nvar):
-            # Create a new subset of x by deleting one column
-            X_sub = np.delete(self.x, i, axis=1)
-            # Apply PolynomialFeatures to the new subset
-            poly_sub = PolynomialFeatures(degree=self.d)
-            X_sub_poly = poly_sub.fit_transform(X_sub)
+            # Fit the model for the current predictor
+            single_model = ElasticNetCV(cv=5, l1_ratio=[.1, .5, .7, .9, .95, .99, 1])
+            single_model.fit(X_s, u)
+            u_s = single_model.predict(X_s)
 
-            # Fit a new model on the transformed subset
-            model_sub = LinearRegression()
-            model_sub.fit(X_sub_poly, self.y)
-            u_sub = model_sub.predict(X_sub_poly)
 
-            # Calculate mu using the coefficients from the new model
-            mu[i] = u_sub
-
-            # Calculate variance explained for each model
-            residual_var = np.mean((self.y - u) ** 2)
-            mu_var = np.mean((self.y - mu[i]) ** 2)
-            psi[i] = 1 - (residual_var / np.var(self.y)) - (1 - (mu_var / np.var(self.y)))
+            # Calculate psi for the current predictor
+            psi1 = 1 - (np.mean((self.y - u) ** 2) / np.var(self.y)) - (1 - np.mean((self.y - u_s) ** 2) / np.var(self.y))
+            psi.append(psi1)
 
         return psi
 
@@ -52,8 +40,8 @@ class vimp:
         X2 = self.x[int(n / 2):]
 
         # Estimating psi1 and psi2
-        v1 = self.get()
-        v2 = self.get()
+        v1 = self.get(y1, X1)
+        v2 = self.get(y2, X2)
 
         psi1 = np.array(v1)
         psi2 = np.array(v2)
@@ -80,4 +68,5 @@ class vimp:
                 tau_q = t
 
         return tau_q, m
+
 
