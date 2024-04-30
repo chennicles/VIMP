813b88fbb4071c21957e7f3233a64da9a23b4ba7
Kyle Chen <164234327+kchen1031@users.noreply.github.com>
2024-04-30 10:15:36 -0400

    Update 100_covariates.py
    Added loop with p=100

--- 100_covariates.py
+++ 100_covariates.py
@@ -1,10 +1,11 @@
 import numpy as np
-from sklearn.preprocessing import PolynomialFeatures
-from sklearn.linear_model import LinearRegression
 import vimp_loop as vloop
+from tqdm import tqdm
+
 
 #Initialize variables
-n = 1000
+n = 500
+loops = 50
 n_cov = 100
 xs = np.random.normal(0,2,n*n_cov)
 X = xs.reshape(n, n_cov)
@@ -17,10 +18,36 @@ I3 = X[:,2] > 0
 
 y = I1*abs(X[:,0]) + I2 + I3 + abs(X[:,5]/4)**3 + abs(X[:,6]/4)**5 + (7/3)*np.cos(X[:,10]/2) + eps
 
+test = vloop.vimp()
+test.get(y, X)
 
-test = vloop.vimp(y, X)
-test.get(5)
 q = 0.3              #Designate FDR level cutoff q
-stats = test.datasplit(q)
+stats = test.datasplit(y,X,q)
 stats[0]
 stats[1]
+
+
+
+n = 100
+loops = 50
+n_cov = 100
+q = 0.3
+cutoffs = []
+mirrors = []
+for i in tqdm(range(loops)):
+    xs = np.random.normal(0, 2, n * n_cov)
+    X = xs.reshape(n, n_cov)
+    eps = np.random.normal(0, 1, n)
+
+
+    y = X[:,0]**2/X[:,1] + X[:,2]**6 + X[:,3] - X[:,4]**3 + X[:,5]**3 + np.cos(X[:,6])/X[:,7] + X[:,8]**3 * abs(X[:,9]/4)**3
+    + X[:,10]**3 + X[:,11]/10 + (10*X[:,12] - 3*X[:,13])/(X[:,14] - X[:,15]) + np.sin(X[:,16]**3) + 15*X[:,17]**2 + X[:,18]/3
+    - X[:,19] + eps
+
+    test = vloop.vimp()
+    stats = test.datasplit(y, X, q)
+
+    cutoffs.append(stats[0])
+    mirrors.append(stats[1])
+
+np.where(sum(mirrors)/50 > n
